64c4edfb9461680ecc06d9a429b303d8581a2ce0
msdzilii <83410300+msdzilii@users.noreply.github.com>
2021-10-01 14:39:18 +0700

    memodifikasi class
    memodifikasi class Nasabah, Individu dan DBHelper

--- ProjectAkhirPBO/src/projectakhirpbo/Nasabah.java
+++ ProjectAkhirPBO/src/projectakhirpbo/Nasabah.java
@@ -1,6 +1,78 @@
-
 package projectakhirpbo;
 
-public class Nasabah {
-    
+import java.util.ArrayList;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public abstract class Nasabah {
+
+    protected IntegerProperty idNasabah;
+    protected StringProperty nama;
+    protected StringProperty alamat;
+    protected ArrayList<Rekening> rekening;
+
+    public Nasabah(Integer idNasabah, String nama, String alamat, ArrayList<Rekening> rekening) {
+        this.idNasabah = new SimpleIntegerProperty(idNasabah);
+        this.nama = new SimpleStringProperty(nama);
+        this.alamat = new SimpleStringProperty(alamat);
+        this.rekening = rekening;
+    }
+
+    public Nasabah(Integer idNasabah, String nama, String alamat, Rekening rek) {
+        rekening = new ArrayList<>();
+        this.idNasabah = new SimpleIntegerProperty(idNasabah);
+        this.nama = new SimpleStringProperty(nama);
+        this.alamat = new SimpleStringProperty(alamat);
+        this.rekening.add(rek);
+    }
+
+    public Integer getIdNasabah() {
+        return idNasabah.get();
+    }
+
+    public void setIdNasabah(Integer idNasabah) {
+        this.idNasabah.set(idNasabah);
+    }
+
+    public String getNama() {
+        return nama.get();
+    }
+
+    public void setNama(String nama) {
+        this.nama.set(nama);
+    }
+
+    public String getAlamat() {
+        return alamat.get();
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat.set(alamat);
+    }
+
+    public ArrayList<Rekening> getRekening() {
+        return rekening;
+    }
+
+    public void setRekening(ArrayList<Rekening> rekening) {
+        this.rekening = rekening;
+    }
+
+    public IntegerProperty getIdNasabahProperty() {
+        return idNasabah;
+    }
+
+    public StringProperty getNamaProperty() {
+        return nama;
+    }
+
+    public StringProperty getAlamatProperty() {
+        return alamat;
+    }
+
+    public void tambahRekening(Rekening rek) {
+        this.getRekening().add(rek);
+    }
 }
--- ProjectAkhirPBO/src/projectakhirpbo/db/DBHelper.java
+++ ProjectAkhirPBO/src/projectakhirpbo/db/DBHelper.java
@@ -1,6 +1,117 @@
-
 package projectakhirpbo.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DBHelper {
-    
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static final String DB = "datanasabah";
+    private static final String MYCONN = "jdbc:mysql://localhost/" + DB;
+    private static final String SQCONN = "jdbc:sqlite:datanasabah.sqlite";
+
+    public static Connection getConnection(String driver) throws SQLException {
+        Connection conn = null;
+        switch (driver) {
+            case "MYSQL": {
+                try {
+                    Class.forName("com.mysql.jdbc.Driver");
+                    conn = DriverManager.getConnection(MYCONN, USER, PASSWORD);
+                    createTable(conn, driver);
+                } catch (ClassNotFoundException ex) {
+                    System.out.println("Library tidak ada");
+                    Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                break;
+            }
+            case "SQLITE": {
+                try {
+                    Class.forName("org.sqlite.JDBC");
+                    conn = DriverManager.getConnection(SQCONN);
+                    createTable(conn, driver);
+                } catch (ClassNotFoundException ex) {
+                    System.out.println("Library tidak ada");
+                    Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                break;
+            }
+        }
+        return conn;
+    }
+
+    public static void createTable(Connection conn, String driver) throws SQLException {
+        String sqlCreate = "";
+
+        switch (driver) {
+            case "MYSQL": {
+                sqlCreate = "CREATE TABLE IF NOT EXISTS `nasabah` ("
+                        + "  `id_nasabah` int(50) NOT NULL,"
+                        + "  `nama` varchar(250) DEFAULT NULL,"
+                        + "  `alamat` varchar(250) DEFAULT NULL,"
+                        + "  PRIMARY KEY (`id_nasabah`)"
+                        + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+                        + "CREATE TABLE IF NOT EXISTS `individu` ("
+                        + "  `nik` bigint(16) NOT NULL,"
+                        + "  `npwp` bigint(16) DEFAULT NULL,"
+                        + "  `id_nasabah` int(50) DEFAULT NULL,"
+                        + "  PRIMARY KEY (`nik`),"
+                        + "  KEY `id_nasabah` (`id_nasabah`),"
+                        + "  FOREIGN KEY (`id_nasabah`) REFERENCES `nasabah` (`id_nasabah`) ON UPDATE CASCADE"
+                        + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+                        + "CREATE TABLE IF NOT EXISTS `perusahaan` ("
+                        + "  `nib` varchar(200) NOT NULL,"
+                        + "  `id_nasabah` int(50) DEFAULT NULL,"
+                        + "  PRIMARY KEY (`nib`),"
+                        + "  KEY `id_nasabah` (`id_nasabah`),"
+                        + "  FOREIGN KEY (`id_nasabah`) REFERENCES `nasabah` (`id_nasabah`) ON UPDATE CASCADE"
+                        + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+                        + "CREATE TABLE IF NOT EXISTS `rekening` ("
+                        + "  `no_rekening` int(50) NOT NULL,"
+                        + "  `saldo` double(100,2) DEFAULT NULL,"
+                        + "  `id_nasabah` int(50) DEFAULT NULL,"
+                        + "  PRIMARY KEY (`no_rekening`),"
+                        + "  KEY `id_nasabah` (`id_nasabah`),"
+                        + "  FOREIGN KEY (`id_nasabah`) REFERENCES `nasabah` (`id_nasabah`) ON UPDATE CASCADE"
+                        + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;";
+                break;
+            }
+            case "SQLITE": {
+                sqlCreate = "CREATE TABLE IF NOT EXISTS nasabah ("
+                        + "    id_nasabah INT (50)      PRIMARY KEY,"
+                        + "    nama       VARCHAR (250),"
+                        + "    alamat     VARCHAR (250) "
+                        + ");"
+                        + "CREATE TABLE IF NOT EXISTS individu ("
+                        + "    nik        BIGINT (16) PRIMARY KEY,"
+                        + "    npwp       BIGINT (16),"
+                        + "    id_nasabah INT (50)    REFERENCES nasabah (id_nasabah) ON DELETE RESTRICT"
+                        + "                                                           ON UPDATE CASCADE"
+                        + ");"
+                        + "CREATE TABLE IF NOT EXISTS perusahaan ("
+                        + "    nib        VARCHAR (200) PRIMARY KEY,"
+                        + "    id_nasabah INT (50)      REFERENCES nasabah (id_nasabah) ON DELETE RESTRICT"
+                        + "                                                             ON UPDATE CASCADE"
+                        + ");"
+                        + "CREATE TABLE IF NOT EXISTS rekening ("
+                        + "    no_rekening INT (50)        PRIMARY KEY,"
+                        + "    saldo       DOUBLE (100, 2),"
+                        + "    id_nasabah  INT (50)        REFERENCES nasabah (id_nasabah) ON DELETE RESTRICT"
+                        + "                                                                ON UPDATE CASCADE"
+                        + ");";
+                break;
+            }
+        }
+
+        String sqls[] = sqlCreate.split(";");
+        for (String sql : sqls) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.execute();
+        }
+
+    }
 }
